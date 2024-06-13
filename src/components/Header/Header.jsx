fe9792885852f7a785e83a104cd9f71e5d238955
nazinamari <93985053+nazinamari@users.noreply.github.com>
2024-06-13 15:28:43 +0100

    theme

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import ThemeSwitcher from '../ThemeSwitcher/ThemeSwitcher';
+
+export default function Header() {
+	return (
+		<div>
+			<ThemeSwitcher />
+		</div>
+	);
+}
---
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -0,0 +1,10 @@
+import Button from '../../shared/Button/Button';
+import css from './ThemeSwitcher.module.css';
+
+export default function ThemeSwitcher() {
+	return (
+		<div>
+			<Button className={css.btn}>1</Button>
+		</div>
+	);
+}
